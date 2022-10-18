b247747c6344eb97630f3fb8bb8506299a35519a
Andrei Bucur <abucur980@gmail.com>
2022-10-18 19:32:37 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Stock from './components/stock/stock.components';
+// import CircularProgress from '@mui/material/CircularProgress';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const App = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [searchList, setSearchList] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(null);
+  const [stockData, setStockData] = useState(null);
+  
+  useEffect(() => {
+    fetch("https://api.stockdata.org/v1/entity/search?search="+searchValue+"&api_token=QmFKRT6W15LPEAr3fxI5UP9FL4dmOlGdHmmRFfnj")
+    .then((rawData) => rawData.json())
+    .then((jsonData) => jsonData.data.filter(data => data.country === 'us' && (data.exchange === "NYSE" || data.exchange === "NASDAQ")))
+    .then((filteredData) => setSearchList(filteredData))
+  }, [searchValue]);
+
+  function returnStock() {
+    if (stockData !== null) {
+      return (<Stock data={stockData}/>)
+    }
+  }
+
+  console.log(searchList);
+
+  // reset the search value and list if dropdown is closed
+  useEffect(() => {
+    if (!open) {
+      setSearchList([]);
+      setSearchValue('');
+    }
+  }, [open]);
+
+  useEffect(() => {
+    if (selectedValue !== null) {
+    fetch("https://api.stockdata.org/v1/data/quote?symbols="+selectedValue.symbol+"&api_token=QmFKRT6W15LPEAr3fxI5UP9FL4dmOlGdHmmRFfnj")
+    .then((rawData) => rawData.json())
+    .then((jsonData) => setStockData(jsonData.data));
+    }
+    console.log(stockData);
+  }, [selectedValue]);
+
+    
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div className="App">
+        <Container maxWidth="sm">
+            <Autocomplete
+              autoComplete
+              open={open}
+              onOpen={() => {
+                setOpen(true);
+              }}
+              onClose={() => {
+                setOpen(false);
+              }}
+              options={searchList}
+              freeSolo
+              getOptionLabel={(option) => option.name}
+              onChange={(event, value) => setSelectedValue(value)}
+              renderOption={(props, option) => {
+                return (
+                  <li {...props} key={option.symbol}>
+                    {option.symbol} --- {option.name}
+                  </li>
+                );
+              }}
+              renderInput={(params) => 
+              <TextField 
+                onChange={(event) => { setSearchValue(event.target.value) }}
+                {...params}
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: (
+                    <React.Fragment>
+                      {params.InputProps.endAdornment}
+                    </React.Fragment>
+                  ),
+                }} 
+                label="What stocks are you looking for?"/>}
+              />
+              {returnStock()}
+        </Container>
+        
+      </div>
+    </ThemeProvider>
+    
+  );
+}
+
+export default App;
---
+++ src/components/stock/stock.components.jsx
@@ -0,0 +1,33 @@
+import "./stock.styles.css";
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
+import Tooltip from '@mui/material/Tooltip';
+
+const Stock = ({data}) => {
+    console.log("data",data);
+    const {ticker, name, exchange_short, price, day_change} = data[0];
+
+    return (
+        <Card elevation={16} sx={{ mt: 4, px:2, mx: 'auto'}}>
+            <Grid container spacing={2}>
+                <Grid item xs={7} md={7}>
+                    <Tooltip title="Company name and symbol">
+                        <h4>{ticker} / {name} -- {exchange_short}</h4>
+                    </Tooltip>
+                </Grid>
+                <Grid item xs={3} md={3}>
+                    <Tooltip title="Current stock price">
+                        <h4>{price} $</h4>
+                    </Tooltip>
+                </Grid>
+                <Grid item xs={2} md={2}>
+                    <Tooltip title="The day change">
+                        <h4>{day_change}%</h4>
+                    </Tooltip>
+                </Grid>
+            </Grid>
+        </Card>
+    )
+}
+
+export default Stock;
